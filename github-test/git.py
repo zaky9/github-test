99f9d37d66538761da47d18405a0fc97c404c40a
zaky9 <zakyr@hotmail.com>
2019-04-25 12:09:50 +0700

    ini ke 3k lll

---
+++ github-test/git.py
@@ -0,0 +1,11 @@
+
+print('hello guys')
+print('Ini ckode kedua')
+print('ini editan ke-3')
+
+# git init --> untuk mulai git
+# git status --> status
+# git add <namafile> --> 
+# git add . --> semua file
+# git commit -m "label" --> selesai projek
+# git log --> melihat commit sebelumnya
